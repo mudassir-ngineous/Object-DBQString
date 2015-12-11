3ff76d3fc4fc6552fc0da4883121af4dc690cfbe
mudassir-ngineous <mudassir@intangles.com>
2015-12-11 23:31:16 +0530

    insert query builder is done for mysql

--- queryBuilder.js
+++ queryBuilder.js
@@ -1,14 +1,52 @@
-"use strict";
-class queryBuilder{
-	constructor(db_name){
-		this.database = db_name;
-	}
+var parseJsonForInsert = function(){
 
-	var getDatabaseName = function(){
-		console.log(this.database);
-	}
 };
 
-var q = queryBuilder("mysql");
+function insertQueryMaker(jsonObj){
+	if(jsonObj.hasOwnProperty("table")){
+		var data = jsonObj.data;
+		var fieldNameStr = "(";
+		var fieldValueStr = "(";
+		for(var field in data){
+			fieldNameStr+=field+",";
+			if(typeof(data[field]) === "number"){
+				fieldValueStr += data[field]+",";
+			}else if(typeof(data[field]) === "string"){
+				fieldValueStr += "\'" + data[field]+"\',";
+			}
+		}
 
-console.log(q.getDatabaseName());
+		if(fieldNameStr[fieldNameStr.length-1] === ","){
+			fieldNameStr[fieldNameStr.length-1] = '';
+		}
+
+		if(fieldValueStr[fieldValueStr.length-1] === ","){
+			fieldValueStr[fieldValueStr.length-1] = '';
+		}
+
+		fieldNameStr = fieldNameStr.slice(0,fieldNameStr.length-1);
+		fieldValueStr = fieldValueStr.slice(0,fieldValueStr.length-1);
+		fieldNameStr+=")";
+		fieldValueStr+=")";
+
+		var finalStr = "INSERT INTO "+ jsonObj.table + " " + fieldNameStr + " VALUES " + fieldValueStr + ";";
+		console.log(finalStr);
+		return finalStr;
+	}
+	else{
+		return null;
+	}
+}
+
+var jsonObj = {
+	table:"emp",
+	data:{
+		id:10,
+		name:"xyz",
+		salary:100000.0,
+		dept:"computer",
+	}
+}
+
+insertQueryMaker(jsonObj);
+	
