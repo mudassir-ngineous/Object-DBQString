d1b6ee88613e483e90cf7f88cc29150a8b801013
mudassir-ngineous <mudassir@intangles.com>
2015-12-27 15:01:01 +0530

    sqlite,sqlserver,postgresql and oracle based files separated

---
+++ test.js
@@ -0,0 +1,16 @@
+var db = require("./queryBuilder.js").init("sqlite");
+
+var jsonObj = {
+	table:"emp",
+	columns:["id","name","salary"],
+	where:{
+		"$or":[{"id":3},{"name":"gopinath"}]
+	},
+	orderbBy:[("id","asc"),("name","desc")],
+	groupBy : ["id","name"],
+	limit:10,
+	distinct:true
+};
+
+var str = db.select(jsonObj);
+console.log(str);
